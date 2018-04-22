2973999f8fa21a4ce753d0bb051d2a9bf9a35e38
ymber <mtaylor1252dev@gmail.com>
2018-04-22 13:25:51 +0100

    packaging update

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python Discord RPC library
+
+This library implements a class to connect to a local Discord RPC server and send rich presence data.
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="discordrpc",
+    version="0.0.0",
+    packages=["discordrpc"],
+    description="Python library for applications connecting to Discord's local RPC server",
+    long_description=open("README.md").read(),
+    author="Matthew Taylor",
+    author_email="mtaylor1252dev@gmail.com",
+    url="https://github.com/ymber/pylibDiscordRPC",
+    license="MIT",
+    python_requires=">=3.6"
+)
